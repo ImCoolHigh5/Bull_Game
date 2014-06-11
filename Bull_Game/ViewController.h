2eee8897712cdb37706e388561485cc7af601915
Jason Dude <imcoolhigh5@gmail.com>
2014-06-11 13:53:42 -0400

    Initial Commit

---
+++ Bull_Game/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bull_Game
+//
+//  Created by Jason Welch on 6/11/14.
+//  Copyright (c) 2014 Stevenson University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Bull_Game/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Bull_Game
+//
+//  Created by Jason Welch on 6/11/14.
+//  Copyright (c) 2014 Stevenson University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
